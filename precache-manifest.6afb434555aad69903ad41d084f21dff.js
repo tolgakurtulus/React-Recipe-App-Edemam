3fcf4f74108b1d18e8441078528fb396d028c4f5
tolga.kurtulus <tolga.kurtulus@odeontechnology.com>
2022-01-30 18:07:26 +0300

    Updates

---
+++ precache-manifest.6afb434555aad69903ad41d084f21dff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b76d9c7e622459759f6806bd976dd8",
+    "url": "/React-Recipe-App-Edemam/index.html"
+  },
+  {
+    "revision": "3a34f9f77ee4316fc57a",
+    "url": "/React-Recipe-App-Edemam/static/css/main.3cfbfa2a.chunk.css"
+  },
+  {
+    "revision": "23d7b84e3845a7e890bb",
+    "url": "/React-Recipe-App-Edemam/static/js/2.1b8ea752.chunk.js"
+  },
+  {
+    "revision": "3a34f9f77ee4316fc57a",
+    "url": "/React-Recipe-App-Edemam/static/js/main.66cc7e7a.chunk.js"
+  },
+  {
+    "revision": "c6972f85aed8994c81f2",
+    "url": "/React-Recipe-App-Edemam/static/js/runtime-main.4eb0d6ba.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React-Recipe-App-Edemam/precache-manifest.6afb434555aad69903ad41d084f21dff.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Recipe-App-Edemam/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
